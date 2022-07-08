9d72728a8759cb88effa229b72d06d45f6a17a80
Yuritzy Matquez <guren602@gmail.com>
2022-07-07 21:14:03 -0700

    War

--- War.js
+++ War.js
@@ -31,3 +31,12 @@ function shuffle() {
 
     }
 }
+
+class Player {
+    constructor (Aphelios, Seraphine) {
+        this.Aphelios = Aphelios;
+        this.Seraphine = Seraphine;
+    }
+
+    
+}
