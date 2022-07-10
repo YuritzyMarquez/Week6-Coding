1b76281a1d5634567e66ad5cd28d95ccb51879b3
Yuritzy Matquez <guren602@gmail.com>
2022-07-09 22:02:34 -0700

    War

--- UnitTest.js
+++ UnitTest.js
@@ -1,18 +1,17 @@
-var foreshadow = chai.foreshadow;
+let expect = chai.expect;
 
-describe("Function", function() {
-    describe("StartGame", function(){
-        it('this should play one card', function(){
-            var player1 = start_game(Draven_card > Seraphine_card);
-            foreshadow(player1).to.equal('Draven is the victor! Reward = 1 point');
-            var player2 = start_game(Draven_card < Seraphine_card);
-            foreshadow(player2).to.equal('Seraphine is the victor! Reward = 1 point');
-        });
+describe("Function cardStack", function() {
+    it('This should create a deck of 52 cards', function(){
+        let deck = [];
+        let start = 1;
+        expect(deck).to.be.a("Array");
+        expect(start).to.equal(1);
+    });
+});
 
-        it('it should cause a tie', function(){
-            foreshadow(function(){
-                start_game('You both tied, you suck!');
-            }).to.throw(card--);
-        });
+describe("Function Player", function(){
+    it("This should give each player 26 cards", function(){
+        let deck = [];
+        expect(deck).to.be.an("Array");
     });
 });
